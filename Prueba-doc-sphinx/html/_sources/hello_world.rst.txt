9bd15b0e9701e51eacd2def0074839340229cf89
Daniel Tomás Sánchez <danieltomas-sanchez-8@hotmail.com>
2020-10-09 23:23:50 +0200

    Añadido codigo de ejemplo y html generado de documentacion

---
+++ Prueba-doc-sphinx/html/_sources/hello_world.rst.txt
@@ -0,0 +1,7 @@
+hello\_world module
+===================
+
+.. automodule:: hello_world
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ Prueba-doc-sphinx/src/hello_world.py
@@ -0,0 +1,18 @@
+
+class ClaseMiaPrueba:
+	"""docstring for ClaseMiaPrueba
+
+    Attributes:
+        msg (str): Human readable string describing the exception.
+
+	"""
+	def __init__(self, msg):
+		self.msg = msg
+		 
+		
+	def hello_world(self):
+		"""Example function with types documented in the docstring.
+
+		"""
+		
+		print("hello world")
