a5580c08a26ebd785034d85354c37a1832be2b83
botadeoro <43742494+botadeoro@users.noreply.github.com>
2020-10-12 10:25:47 +0200

    añadido comentarios a medias

--- prueba-SLY/calc-a-trozos.py
+++ prueba-SLY/calc-a-trozos.py
@@ -3,12 +3,13 @@ from sly import Lexer
 class CalcLexer(Lexer):
     # Set of token names.   This is always required
     tokens = { ID, NUMBER, PLUS, MINUS, TIMES,
-               DIVIDE, ASSIGN, LPAREN, RPAREN }
+               DIVIDE, ASSIGN, LPAREN, RPAREN, COMEN }
 
     # String containing ignored characters between tokens
     ignore = ' \t'
 
     # Regular expression rules for tokens
+    COMEN = r'/\*[a-zA-Z0-9_]*\*/'
     ID      = r'[a-zA-Z][a-zA-Z0-9_]*'
     NUMBER  = r'\d+'
     PLUS    = r'\+'
@@ -19,8 +20,9 @@ class CalcLexer(Lexer):
     LPAREN  = r'\('
     RPAREN  = r'\)'
 
+
 if __name__ == '__main__':
-    data = 'x_Aa= 3 + 42 * (s - t)'
+    data = 'x_Aa= 3 + 42 * (s - t) /*Holi*/'
     lexer = CalcLexer()
     for tok in lexer.tokenize(data):
         print('type=%r, value=%r' % (tok.type, tok.value))
