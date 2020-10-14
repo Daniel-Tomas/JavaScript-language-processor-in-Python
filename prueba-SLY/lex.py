848e7a746f9ba79ec8507cf3568862648f91a260
AaronCaberoBlanco <aaron.cabero.blanco@alumnos.upm.es>
2020-10-14 21:42:28 +0200

    codigo "comentado como buenamente he podido"

--- prueba-SLY/lex.py
+++ prueba-SLY/lex.py
@@ -39,13 +39,36 @@ class CalcLexer(Lexer):
     literals = {'(', ')', '{', '}', ',', ';', '/d'}
 
     def CTE_ENTERA(self, t):
+        """Function called when a token which belongs to an integer constant is found.
+
+        Args:
+            t(token): The only parameter.
+
+        Returns:
+            token: The return value. It's modified to change de value to an Integer value.
+
+        Raises:
+            IOError: If value is bigger than the maximum integer allowed.
+        """
         t.value = int(t.value)
         if t.value > 32767:
             print(f'Número fuera de rango: "{t.value}"')
-            raise IOError
+            exit()
+            #raise IOError
         return t
 
     def CADENA(self, t):
+        """Function called when a token which belongs to an string constant is found.
+
+            Args:
+                t(token): The only parameter.
+
+            Returns:
+                token: The return value which is modified to delete the quotation marks.
+
+            Raises:
+                IOError: if the length of the string is bigger than 64.
+            """
         t.value = t.value[1:-1]
         if len(t.value) > 64:
             print(f'Cadena demasiado larga: "{t.value}"')
@@ -56,6 +79,15 @@ class CalcLexer(Lexer):
     # def ID(self,t):
 
     def OP_ARIT(self, t):
+        """Function called when a token which arithmetical operator is found.
+
+            Args:
+                t(token): The only parameter.
+
+            Returns:
+                token: The return value. It's modified to change de value to an Integer value.
+                This token's integer value will be 0 if "+" is found or 1 if "-"
+        """
         if t.value == '+':
             t.value = 0
         else:
@@ -67,18 +99,21 @@ class CalcLexer(Lexer):
         self.lineno += t.value.count('\n')
 
     def error(self, t):
+        """Function called when a wrong character is found.
+        A character is wrong if it does not belong to a correct token in that very position.
+
+            Args:
+               t(token): The only parameter.
+            Returns:
+                nothing. This method just print the wrong character by standard output.
+        """
         print("Illegal character '%s'" % t.value[0])
         self.index += 1
 
 
 if __name__ == '__main__':
     # data = 'x_Aa= 3 + 42 * (s - t)'
-    data = '''/* Esto es un 
-    comentario */ /*on*/ 
-    /*otro comment*/ "err
-    or" 
-    "puto" string
-     23410 /*on*/ /*on*/fjr =23+2; let'''
+    data = '''2a000'''
     lexer = CalcLexer()
     for tok in lexer.tokenize(data):
         print(f'<{tok.type} , {tok.value}>')
